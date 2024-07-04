b585fe5b3d8449d7a5c5c5681ed98d03b080bf90
Daniel <x00182517@mytudublin.ie>
2024-07-04 08:46:10 +0100

    working site

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     #will affect what is in the fields in the form.py
+    
     age = models.PositiveBigIntegerField(null=True,blank=True)
