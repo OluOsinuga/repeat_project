248c2d47de1ac6559553966f9d9e81fa7f3630c0
Daniel <x00182517@mytudublin.ie>
2024-08-19 20:24:48 +0100

    finshed order history

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'order'
 urlpatterns = [
  path('thanks/<int:order_id>/', views.thanks, name='thanks'),
+ path('history/', views.orderHistory.as_view(), name='order_history'),
+ path('<int:order_id>/',views.orderDetail.as_view(), name='order_detail'),
 ] 
